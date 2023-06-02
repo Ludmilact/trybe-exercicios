8dad25eb239076ff152894e41d42c9eb7bd7ba5c
Ludmilact <ludmilatimoteo@Outlook.com>
2023-06-01 22:42:04 -0300

    exercicio01

---
+++ fundamentos/secao-3-introducao-javaScript/dia-02-javaScript/exercicio01.js
@@ -0,0 +1,6 @@
+const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+
+for (index = 0; index < groceryList.length; index +=1) {
+   let currentItem = groceryList[index]
+    console.log(currentItem);
+}
