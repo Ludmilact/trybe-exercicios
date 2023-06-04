17a2259d7ca86c6b4ab99c76a99d1a7179a70779
Ludmilact <ludmilatimoteo@Outlook.com>
2023-06-03 21:29:10 -0300

    Exercicio05

---
+++ fundamentos/secao-3-introducao-javaScript/dia-02-javaScript/exercicios05.js
@@ -0,0 +1,17 @@
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let sum = 0;
+let elementos = 0;
+let media = 0;
+
+for(index = 0; index < numbers.length; index +=1) {
+    sum += numbers[index];
+    elementos = numbers.length;
+    media = sum / elementos; 
+}
+if (media > 20){
+    console.log("O valor da média aritmética é maior que 20")
+} else {
+    console.log("O valor da média aritmética é menor que 20")
+}
+console.log(media)
