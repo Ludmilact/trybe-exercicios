8bd2b69699e8789b63d11de086957468e149e9f6
Ludmilact <ludmilatimoteo@Outlook.com>
2023-06-05 21:22:48 -0300

    Exercicio 02

--- fundamentos/secao-3-introducao-javaScript/dia-04-javaScript/exercicio01.js
+++ fundamentos/secao-3-introducao-javaScript/dia-04-javaScript/exercicio01.js
@@ -11,6 +11,8 @@ const reader = {
     ],
   };
 
+  //1 - Acesse as chaves name, lastName e title e exiba informações em um console.log() no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Senhor dos Anéis - a Sociedade do Anel’.”.
+
   const message = `O livro favorito de ${reader.name} ${reader.lastName} se chama "${reader.favoriteBooks[0].title}".` 
 
   console.log(message);
