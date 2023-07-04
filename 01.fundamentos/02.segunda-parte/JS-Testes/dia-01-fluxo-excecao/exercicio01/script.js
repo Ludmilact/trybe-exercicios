4bff45c965f960f3d893e83e2cc9ad225b682893
Ludmilact <ludmilatimoteo@Outlook.com>
2023-07-04 11:59:48 -0300

    Exercicios 01 with finally

--- 01.fundamentos/02.segunda-parte/JS-Testes/dia-01-fluxo-excecao/exercicio01/script.js
+++ 01.fundamentos/02.segunda-parte/JS-Testes/dia-01-fluxo-excecao/exercicio01/script.js
@@ -21,24 +21,6 @@ button.addEventListener('click', () => {
   showPromo(name, number);
 });
 
-const showPromo = (name, number) => {
-    const firstText = document.querySelector('#text-initial');
-    const secondText = document.querySelector('#text-final');
-    
-    try {
-      checkName(name);
-      checkNumber(parseInt(number));
-      checkValidateRange(number);
-      const productObject = checkPromo(number);
-  
-      firstText.innerHTML = `Boas-vindas, ${name}!`;
-      secondText.innerHTML = `A promoção do dia é: 
-        ${productObject.product} no valor de R$ ${productObject.price}`;
-    } catch(err) {
-      secondText.innerHTML = err.message;
-    }
-  }
-
   const checkName = (name) => {
     let letters = /[aA-zZ]+/;
     
@@ -63,8 +45,29 @@ const checkPromo = (number) => {
   }
 }
 
-const checkValidateRange = (number) => {
+const checkValidRange = (number) => {
     if (number < 1 || number > 10) {
         throw new Error('Digite um número entre 1 e 10.');
     }
 }
+
+const showPromo = (name, number) => {
+    const firstText = document.querySelector('#text-initial');
+    const secondText = document.querySelector('#text-final');
+    
+    try {
+      checkName(name);
+      checkNumber(parseInt(number));
+      checkValidRange(number);
+      const productObject = checkPromo(number);
+  
+      firstText.innerHTML = `Boas-vindas, ${name}!`;
+      secondText.innerHTML = `A promoção do dia é: 
+        ${productObject.product} no valor de R$ ${productObject.price}`;
+    } catch(err) {
+      secondText.innerHTML = err.message;
+    } finally {
+    document.querySelector('#name-id').value = "";
+    document.querySelector('#number-id').value = "";
+    }
+  }
